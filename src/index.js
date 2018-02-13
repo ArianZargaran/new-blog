b8202d82e11b73ca3b5993d86764b7ca2c15fd61
ArianZargaran <arianzf@gmail.com>
2018-02-12 23:23:55 -0800

    Switch on react-router-dom implemented

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store.js";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import PostsIndex from "./components/posts_index";
+import PostsNew from './components/posts_new';
 
 import registerServiceWorker from "./registerServiceWorker";
 
@@ -13,7 +14,10 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <div>
-        <Route path='/' component={PostsIndex} />
+        <Switch>
+          <Route path='/posts/new' component={PostsNew} />
+          <Route path='/' component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
