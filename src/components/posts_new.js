b50c3cafa84e9456ebcad21aa476d2abb1678001
ArianZargaran <arianzf@gmail.com>
2018-02-16 07:32:00 -0800

    last change

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Field, reduxForm } from "redux-form"
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
   renderField(field){
@@ -11,7 +11,7 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
         />
-        {field.meta.err}
+        {field.meta.error()}
       </div>
     );
   }
